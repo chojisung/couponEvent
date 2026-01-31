f566526d31d49259f39429f24199ad0e3fdbe33f
jisung <jisung1367@gmail.com>
2026-01-31 18:59:01 +0900

    동시성 이슈를 확인하기위해 쿠폰 엔티티 생성

--- src/main/java/com/jisung0509/couponEvent/domain/coupon/Coupon.java
+++ src/main/java/com/jisung0509/couponEvent/domain/coupon/Coupon.java
@@ -1,5 +1,23 @@
 package com.jisung0509.couponEvent.domain.coupon;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Coupon {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @Column(nullable = false)
+  int stock;
 }
